e59c9b0cfea20bc1d5c2cd80c5a84151de5ebb5d
Franco Garcia <francoggarcia96@gmail.com>
2022-08-30 23:31:48 -0300

    Desafio 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import NavBar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer greeting="Tienda Chilli Funko Pop" />
+
     </div>
   );
 }
