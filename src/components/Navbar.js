20111b190326d82b102bcae488410967814e67c0
Franco Garcia <francoggarcia96@gmail.com>
2022-08-30 23:25:22 -0300

    Desafio 2

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+const NavBar = () => {
+    return (
+        <div className='navBarMenu'>
+            <div className='menuButtons'>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Marvel</a></li>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">Otros</a></li>
+                    <li><a href="#">Contacto</a></li>
+                 </ul>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default NavBar;
