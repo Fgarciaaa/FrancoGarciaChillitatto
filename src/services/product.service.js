178fd566db91ecaab169b2aebed636b9a940f654
Franco Garcia <francoggarcia96@gmail.com>
2022-10-06 11:51:32 -0300

    firebase I

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,23 +1,46 @@
-import mock from "./mock";
+import { initializeApp } from "firebase/app";
 
-const DELAY = 2000;
+import {
+  getFirestore,
+  getDocs,
+  collection,
+  query,
+  where,
+  doc,
+  getDoc,
+} from "firebase/firestore";
 
-export function getAllProducts(categoryId) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      if (categoryId) {
-        resolve(mock.filter((product) => product.category === categoryId));
-      } else {
-        resolve(mock);
-      }
-    }, DELAY);
-  });
+const firebaseConfig = {
+  apiKey: "AIzaSyDc3BhD66QddVUrEhbu2IlC3o4rWDUFJyU",
+  authDomain: "chilli-funki.firebaseapp.com",
+  projectId: "chilli-funki",
+  storageBucket: "chilli-funki.appspot.com",
+  messagingSenderId: "386748299061",
+  appId: "1:386748299061:web:0c3187d9463396b1903e7c",
+};
+
+export function initFirebase() {
+  initializeApp(firebaseConfig);
+}
+
+export async function getAllProducts(category) {
+  const db = getFirestore();
+  const queryBase = collection(db, "products");
+  const querySnapshot = category
+    ? query(queryBase, where("category", "==", category))
+    : queryBase;
+
+  return getDocs(querySnapshot).then((response) =>
+    response.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+  );
 }
 
 export function getProductById(id) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(mock.find((x) => x.id === id));
-    }, DELAY);
-  });
+  const db = getFirestore();
+  const queryDoc = doc(db, "products", id);
+
+  return getDoc(queryDoc).then((response) => ({
+    id: response.id,
+    ...response.data(),
+  }));
 }
